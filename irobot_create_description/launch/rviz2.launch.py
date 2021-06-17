70182f1c15586197cd38eb19a54170f7b2500eb9
Rodrigo Causarano <rjcausarano@gmail.com>
2021-06-17 08:21:17 -0400

    Makes spawning in RViz optional and true by delfault

--- irobot_create_description/launch/rviz2.launch.py
+++ irobot_create_description/launch/rviz2.launch.py
@@ -4,7 +4,9 @@ import os
 import launch
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.substitutions import Command
+from launch.substitutions import Command, LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
 from launch_ros.actions import Node
 
 def generate_launch_description():
@@ -19,11 +21,15 @@ def generate_launch_description():
         'rviz',
         'model.rviz')
 
+    run_rviz = DeclareLaunchArgument("rviz", default_value="true",
+                                     description="Start rviz")
+
     rviz = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
         arguments=['-d', rviz_config_dir],
+        condition=IfCondition(LaunchConfiguration("rviz")),
         output='screen')
 
     robot_state_publisher = Node(
@@ -41,7 +47,8 @@ def generate_launch_description():
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
         name='joint_state_publisher_gui',
+        condition=IfCondition(LaunchConfiguration("rviz")),
         output='screen'
     )
 
-    return LaunchDescription([joint_state_publisher, robot_state_publisher, rviz])
+    return LaunchDescription([run_rviz, joint_state_publisher, robot_state_publisher, rviz])
