997c9e06f27a6adce0f6fa91d051fe8b21c4677c
Rodrigo Causarano <46730379+rjcausarano@users.noreply.github.com>
2021-06-17 16:35:44 -0400

    Adds irobot_create_description package (#1)
    * create3_description package with gazebo and rviz included in launch file
    
    * Adds model.rviz
    
    * Address PR comments
    
    * Updates create3 URDF
    
    * Fix TF tree in Rviz
    
    * License moved to root and other minor comments addressed
    
    * Removes traces of gazebo and launches robot in RViz only
    
    * Gives files more appropriate names and makes macro of the caster wheel
    
    * xacro formatting changes and adds README specifying dependencies
    
    * Address comments
    
    * Reformats xacro files and simplifies CMakeLists
    
    * creates numeric variables in xacro files
    
    * revert version number change
    
    * Change variable names
    
    * Makes spawning in RViz optional and true by delfault
    
    * Improvements to launchfile
    
    * updates README and CI config
    
    * Apply suggestions from linters

--- README.md
+++ README.md
@@ -4,12 +4,40 @@
 
 This is a [ROS 2](https://docs.ros.org/en/foxy/index.html) simulation stack for the [iRobot Create 3]() robot.
 
+## Prerequisite
+
+Before moving forward make sure to have either ROS foxy or ROS galactic installed. 
+
 ## Build
 
-TODO(rjcausarano): Explain how to create and build workspace.
+- Create a workspace if you don't already have one:
+
+```bash
+mkdir -p ~/colcon_ws/src
+```
+
+- Clone this repository into the src directory from above.
+
+- Navigate to the workspace and install ros2 dependencies with:
+
+```bash
+cd ~/colcon_ws
+rosdep install --from-path src -yi
+```
+
+- Build the workspace with:
+
+```bash
+colcon build --symlink-install
+source install/local_setup.bash
+```
 
 ## Examples
 
-TODO(rjcausarano): How to execute simulation?
+Create 3 can be spawned in an empty world in RViz with
+
+```bash
+ros2 launch irobot_create_description rviz2.launch.py
+```
 
 ## [Contributions](CONTRIBUTING.md)
---
+++ irobot_create_description/launch/rviz2.launch.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# Copyright 2021 iRobot Corporation. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# @author Rodrigo Jose Causarano Nuñez (rcausaran@irobot.com)
+#
+# Launch Create3 in RViz.
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+
+ARGUMENTS = [
+    DeclareLaunchArgument('rviz', default_value='true',
+                          description='Start rviz')
+]
+
+
+def generate_launch_description():
+    pkg_create3_description = get_package_share_directory('irobot_create_description')
+
+    xacro_file = PathJoinSubstitution([pkg_create3_description, 'urdf', 'create3.urdf.xacro'])
+
+    # Rviz
+    rviz_config_dir = PathJoinSubstitution([pkg_create3_description, 'rviz', 'model.rviz'])
+
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config_dir],
+        condition=IfCondition(LaunchConfiguration('rviz')),
+        output='screen')
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[
+            {'use_sim_time': True},
+            {'robot_description': Command(['xacro', ' ', xacro_file])}])
+
+    joint_state_publisher = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        condition=IfCondition(LaunchConfiguration('rviz')),
+        output='screen')
+
+    # Define LaunchDescription variable
+    ld = LaunchDescription(ARGUMENTS)
+    # Add nodes to LaunchDescription
+    ld.add_action(joint_state_publisher)
+    ld.add_action(robot_state_publisher)
+    ld.add_action(rviz)
+
+    return ld
