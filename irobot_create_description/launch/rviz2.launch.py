a78b4c59397247f22ca4e6fb1ef60794dc86c6a1
Rodrigo Causarano <rjcausarano@gmail.com>
2021-06-18 16:22:52 -0400

    Address comments

--- irobot_create_description/launch/rviz2.launch.py
+++ irobot_create_description/launch/rviz2.launch.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# @author Rodrigo Jose Causarano Nuñez (rcausaran@irobot.com)
+# @author Rodrigo Jose Causarano Nunez (rcausaran@irobot.com)
 #
 # Launch Create3 in RViz.
 
