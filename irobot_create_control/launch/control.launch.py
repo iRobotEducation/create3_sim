a00cc37cc82a80851666d0f57d27ff3fed7f92f8
Rodrigo Causarano <rjcausarano@gmail.com>
2021-07-12 08:18:00 -0400

    fix flake 8 erros

--- irobot_create_control/launch/control.launch.py
+++ irobot_create_control/launch/control.launch.py
@@ -17,8 +17,7 @@
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, RegisterEventHandler
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessExit
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
