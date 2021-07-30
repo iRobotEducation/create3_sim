e85364b24a1f4514d5db93cee3f02f6c6abe3e5e
Alexis Pojomovsky <apojomovsky@ekumenlabs.com>
2021-07-30 17:04:40 -0400

    Fixed github actions failures

--- irobot_create_gazebo_plugins/include/irobot_create_gazebo_plugins/gazebo_ros_bumper.hpp
+++ irobot_create_gazebo_plugins/include/irobot_create_gazebo_plugins/gazebo_ros_bumper.hpp
@@ -1,12 +1,22 @@
 // Copyright 2021 iRobot, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// @author Alexis Pojomovsky (apojomovskyt @irobot.com)
 
 #pragma once
 
 #include <cmath>
-#include <memory>
-#include <string>
-#include <tuple>
-
 #include <gazebo/common/Plugin.hh>
 #include <gazebo/gazebo.hh>
 #include <gazebo/msgs/msgs.hh>
@@ -15,16 +25,15 @@
 #include <gazebo/transport/transport.hh>
 #include <gazebo_ros/conversions/builtin_interfaces.hpp>
 #include <gazebo_ros/node.hpp>
-
 #include <irobot_create_msgs/msg/hazard_detection.hpp>
+#include <memory>
+#include <string>
+#include <tuple>
 
 namespace irobot_gazebo_plugins
 {
 /// \brief Convert radians to degradians
-inline int Rad2Deg(double radians)
-{
-  return radians / M_PI * 180;
-}
+inline int Rad2Deg(double radians) { return radians / M_PI * 180; }
 
 /**
  * https://stackoverflow.com/a/11412077
@@ -38,18 +47,14 @@ bool IsAngleBetween(double target, double angle1, double angle2)
 
   // make the angle from a1 to a2 to be <= 180 degrees
   int r = ((a2 - a1) % 360 + 360) % 360;
-  if (r >= 180)
-  {
+  if (r >= 180) {
     std::swap(a1, a2);
   }
 
   // check if it passes through zero
-  if (a1 <= a2)
-  {
+  if (a1 <= a2) {
     return (t >= a1) && (t <= a2);
-  }
-  else
-  {
+  } else {
     return (t >= a1) || (t <= a2);
   }
 }
@@ -75,14 +80,7 @@ protected:
 
 private:
   // Bumper zones
-  enum class BumperZone
-  {
-    RIGHT,
-    CENTER_RIGHT,
-    CENTER,
-    CENTER_LEFT,
-    LEFT
-  };
+  enum class BumperZone { RIGHT, CENTER_RIGHT, CENTER, CENTER_LEFT, LEFT };
 
   // Auxiliar data structure to hold bumper zone details
   struct BumperZoneDefinition
@@ -94,18 +92,17 @@ private:
 
   // Data structure to hold the definitions related to bumper zones
   const std::map<BumperZone, BumperZoneDefinition> bumper_angles_map = {
-    { BumperZone::RIGHT, { -M_PI / 2, -3 * M_PI / 10, "right" } },
-    { BumperZone::CENTER_RIGHT, { -3 * M_PI / 10, -M_PI / 10, "center_right" } },
-    { BumperZone::CENTER, { -M_PI / 10, M_PI / 10, "center" } },
-    { BumperZone::CENTER_LEFT, { M_PI / 10., 3 * M_PI / 10, "center_left" } },
-    { BumperZone::LEFT, { 3 * M_PI / 10, M_PI / 2, "left" } }
-  };
+    {BumperZone::RIGHT, {-M_PI / 2, -3 * M_PI / 10, "right"}},
+    {BumperZone::CENTER_RIGHT, {-3 * M_PI / 10, -M_PI / 10, "center_right"}},
+    {BumperZone::CENTER, {-M_PI / 10, M_PI / 10, "center"}},
+    {BumperZone::CENTER_LEFT, {M_PI / 10., 3 * M_PI / 10, "center_left"}},
+    {BumperZone::LEFT, {3 * M_PI / 10, M_PI / 2, "left"}}};
 
   // Pointer to ros node
   gazebo_ros::Node::SharedPtr rosnode_;
 
   // ROS publisher for the bumper output
-  rclcpp::Publisher<irobot_create_msgs::msg::HazardDetection>::SharedPtr bumper_pub_{ nullptr };
+  rclcpp::Publisher<irobot_create_msgs::msg::HazardDetection>::SharedPtr bumper_pub_{nullptr};
 
   // Transformation between robot pose and world
   ignition::math::Matrix4d r_tf_w_;
@@ -123,13 +120,13 @@ private:
   irobot_create_msgs::msg::HazardDetection msg_;
 
   // Gazebo transport node
-  gazebo::transport::NodePtr gz_node_{ nullptr };
+  gazebo::transport::NodePtr gz_node_{nullptr};
 
   // Gazebo subscriber to pose topic
-  gazebo::transport::SubscriberPtr gz_sub_{ nullptr };
+  gazebo::transport::SubscriberPtr gz_sub_{nullptr};
 
   // Gazebo transport callback to retrieve the robot's pose
-  void GzPoseCallback(ConstPosesStampedPtr &msg);
+  void GzPoseCallback(ConstPosesStampedPtr & msg);
 };
 
 }  // namespace irobot_gazebo_plugins
--- irobot_create_gazebo_plugins/src/gazebo_ros_bumper.cpp
+++ irobot_create_gazebo_plugins/src/gazebo_ros_bumper.cpp
@@ -1,20 +1,35 @@
-#include <irobot_create_gazebo_plugins/gazebo_ros_bumper.h>
+// Copyright 2021 iRobot, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// @author Alexis Pojomovsky (apojomovsky@irobot.com)
+
+#include <irobot_create_gazebo_plugins/gazebo_ros_bumper.hpp>
 
 namespace irobot_gazebo_plugins
 {
 void GazeboRosBumper::Load(gazebo::sensors::SensorPtr sensor, sdf::ElementPtr sdf)
 {
   bumper_ = std::dynamic_pointer_cast<gazebo::sensors::ContactSensor>(sensor);
-  if (!bumper_)
-  {
+  if (!bumper_) {
     gzerr << "Bumper Contact Plugin requires a Contact Sensor.\n";
     return;
   }
   rosnode_ = gazebo_ros::Node::Get(sdf);
   const std::string bumper_topic_name = "/bumper";
-  const gazebo_ros::QoS &qos = rosnode_->get_qos();
+  const gazebo_ros::QoS & qos = rosnode_->get_qos();
   bumper_pub_ = rosnode_->create_publisher<irobot_create_msgs::msg::HazardDetection>(
-      bumper_topic_name, qos.get_publisher_qos(bumper_topic_name, rclcpp::SensorDataQoS()));
+    bumper_topic_name, qos.get_publisher_qos(bumper_topic_name, rclcpp::SensorDataQoS()));
 
   // Listen to the update event.
   update_connection_ = bumper_->ConnectUpdated(boost::bind(&GazeboRosBumper::OnUpdate, this));
@@ -35,13 +50,12 @@ void GazeboRosBumper::OnUpdate()
   gazebo::msgs::Contacts contacts;
   contacts = bumper_->Contacts();
 
-  for (int i = 0; i < contacts.contact_size(); ++i)
-  {
+  for (int i = 0; i < contacts.contact_size(); ++i) {
     // Get collision point in world reference frame
-    const ignition::math::Vector3d c_vec = gazebo::msgs::ConvertIgn(contacts.contact(i).position(0));
+    const ignition::math::Vector3d c_vec =
+      gazebo::msgs::ConvertIgn(contacts.contact(i).position(0));
     // Get collision w.r.t. robot frame
-    if (r_tf_w_ == ignition::math::Matrix4d::Zero)
-    {
+    if (r_tf_w_ == ignition::math::Matrix4d::Zero) {
       RCLCPP_WARN_STREAM(rosnode_->get_logger(), "Global pose callback is not being invoked");
       return;
     }
@@ -50,10 +64,10 @@ void GazeboRosBumper::OnUpdate()
     // Check what zone of the bumper has bumped
     // Only publish if the bump event corresponds to one of the zones
     // "released" events are not publsihed.
-    for (auto &bumper_zone : bumper_angles_map)
-    {
-      if (IsAngleBetween(relative_contact_angle_xy, bumper_zone.second.left_limit, bumper_zone.second.right_limit))
-      {
+    for (auto & bumper_zone : bumper_angles_map) {
+      if (IsAngleBetween(
+            relative_contact_angle_xy, bumper_zone.second.left_limit,
+            bumper_zone.second.right_limit)) {
         // Fill and publish a message
         msg_.header.frame_id = bumper_zone.second.name;
         msg_.type = irobot_create_msgs::msg::HazardDetection::BUMP;
@@ -64,21 +78,20 @@ void GazeboRosBumper::OnUpdate()
   }
 }
 
-void GazeboRosBumper::GzPoseCallback(ConstPosesStampedPtr &msg)
+void GazeboRosBumper::GzPoseCallback(ConstPosesStampedPtr & msg)
 {
   // Proactively update the message's timestamp
-  msg_.header.stamp = gazebo_ros::Convert<builtin_interfaces::msg::Time>(bumper_->LastMeasurementTime());
+  msg_.header.stamp =
+    gazebo_ros::Convert<builtin_interfaces::msg::Time>(bumper_->LastMeasurementTime());
 
   // Iterate over the pose's msg
-  for (auto &pose : msg->pose())
-  {
+  for (auto & pose : msg->pose()) {
     // Stop when the create3 is found, since it's the one in world coordinates.
-    if (pose.name() == "create3")
-    {
+    if (pose.name() == "create3") {
       // Update the global pose object
-      r_tf_w_ = ignition::math::Matrix4d(
-          ignition::math::Pose3d(pose.position().x(), pose.position().y(), pose.position().z(), pose.orientation().w(),
-                                 pose.orientation().x(), pose.orientation().y(), pose.orientation().z()));
+      r_tf_w_ = ignition::math::Matrix4d(ignition::math::Pose3d(
+        pose.position().x(), pose.position().y(), pose.position().z(), pose.orientation().w(),
+        pose.orientation().x(), pose.orientation().y(), pose.orientation().z()));
       return;
     }
   }
