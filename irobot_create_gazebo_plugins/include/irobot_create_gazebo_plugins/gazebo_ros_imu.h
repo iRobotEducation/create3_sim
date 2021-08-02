7e6607de54cce0032e7047960944cc9fb23efd35
Rodrigo Causarano <rjcausarano@gmail.com>
2021-08-02 16:52:51 -0400

    Adds IMU sensor

---
+++ irobot_create_gazebo_plugins/include/irobot_create_gazebo_plugins/gazebo_ros_imu.h
@@ -0,0 +1,57 @@
+// Copyright 2021 iRobot, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// @author Rodrigo Jose Causarano Nunez (rcausaran@irobot.com)
+
+#pragma once
+
+#include <gazebo/plugins/ImuSensorPlugin.hh>
+#include <gazebo_ros/conversions/builtin_interfaces.hpp>
+#include <gazebo_ros/conversions/geometry_msgs.hpp>
+#include <gazebo_ros/utils.hpp>
+#include <sensor_msgs/msg/imu.hpp>
+#include <irobot_create_gazebo_plugins/gazebo_ros_helpers.hpp>
+
+namespace irobot_create_gazebo_plugins
+{
+/// Plugin to attach to a gazebo IMU sensor and publish ROS message of output
+class GazeboRosImu : public gazebo::SensorPlugin
+{
+public:
+  /// Constructor.
+  GazeboRosImu();
+  /// Destructor.
+  virtual ~GazeboRosImu();
+
+  // Documentation Inherited
+  void Load(gazebo::sensors::SensorPtr _sensor, sdf::ElementPtr _sdf) override;
+
+private:
+
+  /// Node for ros communication
+  gazebo_ros::Node::SharedPtr ros_node_;
+  /// Publish for imu message
+  rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr pub_;
+  /// IMU message modified each update
+  sensor_msgs::msg::Imu::SharedPtr msg_;
+  /// IMU sensor this plugin is attached to
+  gazebo::sensors::ImuSensorPtr sensor_;
+  /// Event triggered when sensor updates
+  gazebo::event::ConnectionPtr sensor_update_event_;
+
+  /// Publish latest imu data to ROS
+  void OnUpdate();
+};
+
+}  // namespace irobot_create_gazebo_plugins
---
+++ irobot_create_gazebo_plugins/src/gazebo_ros_imu.cpp
@@ -0,0 +1,78 @@
+// Copyright 2021 iRobot, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// @author Rodrigo Jose Causarano Nunez (rcausaran@irobot.com)
+
+#include <irobot_create_gazebo_plugins/gazebo_ros_imu.h>
+
+namespace irobot_create_gazebo_plugins
+{
+
+GazeboRosImu::GazeboRosImu()
+{
+}
+
+GazeboRosImu::~GazeboRosImu()
+{
+}
+
+void GazeboRosImu::Load(gazebo::sensors::SensorPtr _sensor, sdf::ElementPtr _sdf)
+{
+  ros_node_ = gazebo_ros::Node::Get(_sdf);
+
+  // Get QoS profiles
+  const gazebo_ros::QoS & qos = ros_node_->get_qos();
+
+  sensor_ = std::dynamic_pointer_cast<gazebo::sensors::ImuSensor>(_sensor);
+  if (!sensor_) {
+    RCLCPP_ERROR(ros_node_->get_logger(), "Parent is not an imu sensor. Exiting.");
+    return;
+  }
+
+  sensor_->SetWorldToReferenceOrientation(ignition::math::Quaterniond::Identity);
+
+  pub_ =
+    ros_node_->create_publisher<sensor_msgs::msg::Imu>("~/out", qos.get_publisher_qos("~/out", rclcpp::SensorDataQoS()));
+
+  // Create message to be reused
+  auto msg = std::make_shared<sensor_msgs::msg::Imu>();
+
+  // Get frame for message
+  msg->header.frame_id = gazebo_ros::SensorFrameID(*_sensor, *_sdf);
+
+  msg_ = msg;
+
+  sensor_update_event_ = sensor_->ConnectUpdated(
+    std::bind(&GazeboRosImu::OnUpdate, this));
+}
+
+void GazeboRosImu::OnUpdate()
+{
+  // Fill message with latest sensor data
+  msg_->header.stamp = gazebo_ros::Convert<builtin_interfaces::msg::Time>(
+    sensor_->LastUpdateTime());
+  msg_->orientation =
+    gazebo_ros::Convert<geometry_msgs::msg::Quaternion>(sensor_->Orientation());
+  msg_->angular_velocity = gazebo_ros::Convert<geometry_msgs::msg::Vector3>(
+    sensor_->AngularVelocity());
+  msg_->linear_acceleration = gazebo_ros::Convert<geometry_msgs::msg::Vector3>(
+    sensor_->LinearAcceleration());
+  // Publish message
+  pub_->publish(*msg_);
+
+}
+
+GZ_REGISTER_SENSOR_PLUGIN(GazeboRosImu)
+
+}  // namespace irobot_create_gazebo_plugins
