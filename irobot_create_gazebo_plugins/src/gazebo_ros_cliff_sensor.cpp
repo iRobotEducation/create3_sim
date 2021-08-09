c97fdaaccaf246e1f62e3b8521e773cbe89a1447
lchico <lchico85@gmail.com>
2021-08-09 11:02:36 -0300

    Add cliff sensors (#9)
    * Use realistic wheel drop model (#8)
    
    * Use realistic wheeldrop model
    
    * Rename parent argument to parent_link
    
    * Add wheel drop plugin
    
    * Enable C++ linter
    
    * Fix linter
    
    * Create irobot_create_gazebo_plugins
    
    * Add env hooks
    
    * Fix reminiscents of irobot_create_gazebo_plugins
    
    * Address Alexis' comments
    
    * Use 62 Hz by default
    
    * Fix body inclination offset
    
    * Fix plugin installation
    
    * Remove env-hooks
    
    * Address Rodri's comment
    
    Add urdf cliff sensor to create3
    
    Add cliff plugin
    
    Addressed Emi and Rodr's comments
    
    Enable linter
    
    Address Emi Rodri and Alexis comments
    
    Update irobot_create_description/urdf/cliffs_sensor.urdf.xacro
    
    Co-authored-by: Emiliano Borghi <emiliano.borghi@gmail.com>
    
    Address more comments
    
    Update cliff sensor links and added to the create3
    
    Fix remapping warning
    
    Update ament checks  and emi comments
    
    Addressed Emi's comments
    
    Address more Emi's commment
    
    Update cliff topic
    
    Address Emi's functions
    
    Improve the code with Emi comments
    
    * Fix rebase issue
    
    * Address Rodri comments
    
    * Update argument to be camel_cased
    
    * Add always_on flag
    
    * Update irobot_create_description/urdf/create3.urdf.xacro
    
    Co-authored-by: Emiliano Borghi <creativa_eborghi@irobot.com>
    
    * Update irobot_create_description/urdf/sensors/cliff_sensor.urdf.xacro
    
    Co-authored-by: Emiliano Borghi <creativa_eborghi@irobot.com>
    
    * Update irobot_create_gazebo_plugins/CMakeLists.txt
    
    Co-authored-by: Emiliano Borghi <creativa_eborghi@irobot.com>
    
    * Update irobot_create_gazebo_plugins/src/gazebo_ros_cliff_sensor.cpp
    
    Co-authored-by: Emiliano Borghi <creativa_eborghi@irobot.com>
    
    * Last Emi comments
    
    * Missing comments
    
    * Update irobot_create_description/urdf/sensors/cliff_sensor.urdf.xacro
    
    Co-authored-by: Rodrigo Causarano <46730379+rjcausarano@users.noreply.github.com>
    
    * Update rodri comments
    
    * Address Alexis comments
    
    * Address Alexis comments
    
    * Address Alexis comments
    
    * Fix clang format
    
    Co-authored-by: Emiliano Borghi <creativa_eborghi@irobot.com>
    Co-authored-by: Luis Chico <lchico@ekumenlabs.com>
    Co-authored-by: Rodrigo Causarano <46730379+rjcausarano@users.noreply.github.com>

---
+++ irobot_create_gazebo_plugins/src/gazebo_ros_cliff_sensor.cpp
@@ -0,0 +1,79 @@
+// Copyright 2021 iRobot, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// @author Luis Enrique Chico Capistrano (lchico@irobot.com)
+
+#include <irobot_create_gazebo_plugins/gazebo_ros_cliff_sensor.hpp>
+
+namespace irobot_create_gazebo_plugins
+{
+// Register this plugin with the simulator
+GZ_REGISTER_SENSOR_PLUGIN(GazeboRosCliffSensor)
+
+// Load the controller
+void GazeboRosCliffSensor::Load(gazebo::sensors::SensorPtr parent, sdf::ElementPtr sdf)
+{
+  // Get the world name.
+  const std::string world_name_ = parent->WorldName();
+  world_ = gazebo::physics::get_world(world_name_);
+
+  cliff_sensor_ = std::dynamic_pointer_cast<gazebo::sensors::RaySensor>(parent);
+  GZ_ASSERT(cliff_sensor_, "GazeboRosCliffSensor controller requires a Ray Sensor as its parent");
+
+  utils::initialize(detection_threshold_, sdf, "detection_threshold", 0.01);
+  // Configure our static header message
+  utils::initialize(msg_.header.frame_id, sdf, "frame_id", "");
+
+  // Create a GazeboRos node instead of a common ROS node.
+  // Pass it SDF parameters so common options like namespace and remapping can be handled.
+  ros_node_ = gazebo_ros::Node::Get(sdf);
+
+  // Initialize ROS publisher
+  pub_ = ros_node_->create_publisher<irobot_create_msgs::msg::HazardDetection>(
+    "~/out", rclcpp::SensorDataQoS());
+
+  new_laser_scans_connection_ = cliff_sensor_->LaserShape()->ConnectNewLaserScans(
+    std::bind(&GazeboRosCliffSensor::OnNewLaserScans, this));
+
+  // Configure our static message charasteristics
+  max_range_ = cliff_sensor_->RangeMax();
+
+  RCLCPP_INFO(ros_node_->get_logger(), "Started plugin");
+}
+
+// Function is called when the world is resetted
+void GazeboRosCliffSensor::Reset() { new_laser_scans_connection_.reset(); }
+
+// Update the plugin
+void GazeboRosCliffSensor::OnNewLaserScans()
+{
+  // Configure an empty message with the timestamp
+  msg_.header.stamp = gazebo_ros::Convert<builtin_interfaces::msg::Time>(world_->SimTime());
+
+  // Find the minimum detected distance
+  std::vector<double> ranges;
+  cliff_sensor_->Ranges(ranges);
+  const double range_detection = std::min(utils::FindMinimumRange(ranges), max_range_);
+  RCLCPP_DEBUG_STREAM(ros_node_->get_logger(), "Cliff reporting " << range_detection << " m");
+
+  if (range_detection >= detection_threshold_) {
+    msg_.type = msg_.CLIFF;
+    // Publish message
+    pub_->publish(msg_);
+    RCLCPP_DEBUG_EXPRESSION(
+      ros_node_->get_logger(), true, "Cliff %s ON: %.3f", msg_.header.frame_id.c_str(),
+      range_detection);
+  }
+}
+}  // namespace irobot_create_gazebo_plugins
