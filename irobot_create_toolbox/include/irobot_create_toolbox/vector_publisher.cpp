f690bae83170fee8538fe777d6c15913cfa4ebfc
Rodrigo Causarano <rjcausarano@gmail.com>
2021-08-11 18:32:45 -0400

    Adds add_msg and clear_msgs functions

---
+++ irobot_create_toolbox/include/irobot_create_toolbox/vector_publisher.cpp
@@ -0,0 +1,63 @@
+// Copyright 2021 iRobot, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// @author Rodrigo Jose Causarano Nunez (rcausaran@irobot.com)
+
+template<class T, class V>
+VectorPublisher<T, V>::VectorPublisher(std::string publisher_topic, std::vector<std::string> subscription_topics) : Node("vector_publisher")
+{
+  publisher_ = this->create_publisher<V>(publisher_topic, rclcpp::SensorDataQoS());
+
+  std::cout << subscription_topics[0] << std::endl;
+
+  const float frequency{62.0};  // Hz
+  timer_ = this->create_wall_timer(
+    std::chrono::duration<float>(1 / frequency),
+    std::bind(&VectorPublisher::publisher_callback, this));
+
+
+  // Create subscriptions
+  for(std::string topic : subscription_topics) subs_vector_.push_back(this->create_subscription<T>(topic, rclcpp::SensorDataQoS(), std::bind(&VectorPublisher::subscription_callback, this, std::placeholders::_1)));
+}
+
+template<class T, class V>
+void VectorPublisher<T, V>::subscription_callback(std::shared_ptr<T> msg)
+{
+  std::lock_guard<std::mutex> lock{mutex_};
+
+  add_msg(msg);
+}
+
+template<class T, class V>
+void VectorPublisher<T, V>::publisher_callback()
+{
+  std::lock_guard<std::mutex> lock{mutex_};
+
+  // Publish detected vector.
+  publisher_->publish(msg_);
+
+  clear_msgs();
+}
+
+template<class T, class V>
+void VectorPublisher<T, V>::add_msg(std::shared_ptr<T> msg){
+  // Add message to vector.
+  msg_.detections.push_back(*msg);
+}
+
+template<class T, class V>
+void VectorPublisher<T, V>::clear_msgs(){
+  // Clear the vector now that it was published.
+  msg_.detections.clear();
+}
