70e0b226728388bc0fd7f7362cb4771f6e96af92
Rodrigo Causarano <rjcausarano@gmail.com>
2021-08-18 14:50:09 -0400

    Successful subscription to dynamic_joint_states

---
+++ irobot_create_toolbox/include/irobot_create_toolbox/wheels_publisher.hpp
@@ -0,0 +1,45 @@
+// Copyright 2021 iRobot, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// @author Rodrigo Jose Causarano Nunez (rcausaran@irobot.com)
+
+#pragma once
+
+#include <irobot_create_msgs/msg/wheel_ticks.hpp>
+#include <irobot_create_msgs/msg/wheel_vels.hpp>
+#include <control_msgs/msg/dynamic_joint_state.hpp>
+#include <std_msgs/msg/string.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+class WheelsPublisher : public rclcpp::Node
+{
+public:
+  /// \brief Constructor
+  WheelsPublisher();
+  void subscription_callback(const control_msgs::msg::DynamicJointState::SharedPtr msg);
+  void publisher_callback();
+
+private:
+  rclcpp::TimerBase::SharedPtr timer_;
+  double last_right_angular_vel_;
+  double last_left_angular_vel_;
+  irobot_create_msgs::msg::WheelVels angular_vels_msg_;
+  irobot_create_msgs::msg::WheelTicks wheel_ticks_msg_;
+  std::shared_ptr<rclcpp::Publisher<irobot_create_msgs::msg::WheelVels>> angular_vels_publisher_;
+  std::shared_ptr<rclcpp::Publisher<irobot_create_msgs::msg::WheelTicks>> wheel_ticks_publisher_;
+  std::shared_ptr<rclcpp::Subscription<control_msgs::msg::DynamicJointState>> subscription_;
+  // Mutex
+  std::mutex mutex_;
+
+};
---
+++ irobot_create_toolbox/src/wheels_publisher.cpp
@@ -0,0 +1,51 @@
+// Copyright 2021 iRobot, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// @author Rodrigo Jose Causarano Nunez (rcausaran@irobot.com)
+
+#include <irobot_create_toolbox/wheels_publisher.hpp>
+
+WheelsPublisher::WheelsPublisher() : rclcpp::Node("wheels_publisher_node")
+{
+  angular_vels_publisher_ = this->create_publisher<irobot_create_msgs::msg::WheelVels>("wheel_vels", rclcpp::SystemDefaultsQoS());
+  wheel_ticks_publisher_ = this->create_publisher<irobot_create_msgs::msg::WheelTicks>("wheel_ticks", rclcpp::SystemDefaultsQoS());
+
+  const double frequency{62.0};  // Hz
+  timer_ = this->create_wall_timer(
+    std::chrono::duration<double>(1 / frequency),
+    std::bind(&WheelsPublisher::publisher_callback, this));
+
+  subscription_ = this->create_subscription<control_msgs::msg::DynamicJointState>("dynamic_joint_states", rclcpp::SystemDefaultsQoS(), std::bind(&WheelsPublisher::subscription_callback, this, std::placeholders::_1));
+}
+
+void WheelsPublisher::subscription_callback(const control_msgs::msg::DynamicJointState::SharedPtr msg){
+  RCLCPP_DEBUG(this->get_logger(), "Updating wheel interface variables");
+
+  {  // Limit the scope of the mutex for good practice.
+    std::lock_guard<std::mutex> lock{mutex_};
+    // TODO: Make sure the indexes below are not error prone, if they are then find a way to always obtain
+    // the desired wheel independent of the indexes.
+    last_right_angular_vel_ = msg->interface_values[0].values[1];  // index [0].[1] refers to the right wheel angular velocity
+    last_left_angular_vel_ = msg->interface_values[1].values[1];  // index [1].[1] refers to the left wheel angular velocity
+  }
+}
+
+void WheelsPublisher::publisher_callback(){
+  RCLCPP_INFO(this->get_logger(), "publisher is working");
+  {  // Limit the scope of the mutex for good practice.
+    std::lock_guard<std::mutex> lock{mutex_};
+    std::cout << "left wheel angular vel: " << last_left_angular_vel_ << std::endl;
+    std::cout << "right wheel angular vel: " << last_right_angular_vel_ << std::endl;
+  }
+}
