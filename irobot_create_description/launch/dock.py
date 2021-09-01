4b24b0f0802071a7e1110845ebeff02535b5459a
Rodrigo Causarano <rjcausarano@gmail.com>
2021-09-01 14:01:05 -0400

    less error prone included launch files

--- irobot_create_description/launch/dock.py
+++ irobot_create_description/launch/dock.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # Copyright 2021 iRobot Corporation. All Rights Reserved.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
