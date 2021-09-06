a901bdbfb52b446acb9cf563e6c0155598c61c22
Luis Chico <lchico@ekumenlabs.com>
2021-09-06 15:56:17 -0300

    Address Emi's comments

--- irobot_create_gazebo/launch/aws_small.launch.py
+++ irobot_create_gazebo/launch/aws_small.launch.py
@@ -12,47 +12,46 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # @author Luis Enrique Chico Capistrano (lchico@irobot.com)
-#
-# Launch Create3 in Gazebo and optionally also in RViz.
-
-import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import EnvironmentVariable, PathJoinSubstitution
 
-ARGUMENTS = [
-    # Set the robot and dock position close to the wall by default
-    DeclareLaunchArgument('x', default_value='9.0',
-                          description='Update "x" component of the robot pose.')
-]
+ARGUMENTS = []
+
+# Set the robot and dock pose close to the wall by default
+for pose_element, default_value in zip(['x', 'y', 'yaw'], ['9.0', '0.0', '0.0']):
+    ARGUMENTS.append(DeclareLaunchArgument(pose_element, default_value=default_value,
+                     description=f'{pose_element} component of the robot pose.'))
 
 
 def generate_launch_description():
     # Directories
-    pkg_aws_house_dir = get_package_share_directory('aws_robomaker_small_house_world')
-    pkg_create3_gazebo = get_package_share_directory('irobot_create_gazebo')
+    aws_small_house_dir = get_package_share_directory('aws_robomaker_small_house_world')
+    irobot_create_gazebo_dir = get_package_share_directory('irobot_create_gazebo')
 
     # Paths
     create3_launch_file = PathJoinSubstitution(
-        [pkg_create3_gazebo, 'launch', 'create3.launch.py'])
-
-    world_name_str = 'small_house.world'
-    world_path = os.path.join(pkg_aws_house_dir, 'worlds', world_name_str)
+        [irobot_create_gazebo_dir, 'launch', 'create3.launch.py'])
+    world_path = PathJoinSubstitution([aws_small_house_dir, 'worlds', 'small_house.world'])
+    model_path = PathJoinSubstitution([aws_small_house_dir, 'models'])
 
     # Includes
-    create3_gazebo = IncludeLaunchDescription(
+    world_spawn = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([create3_launch_file]),
-        launch_arguments={'world_path': world_path}.items()
-    )
+        launch_arguments={'world_path': world_path}.items())
+
+    # Add AWS models to gazebo path
+    set_gazebo_model_path_env = SetEnvironmentVariable(
+        name='GAZEBO_MODEL_PATH',
+        value=[EnvironmentVariable('GAZEBO_MODEL_PATH'), model_path])
 
     # Define LaunchDescription variable
     ld = LaunchDescription(ARGUMENTS)
-    # Add nodes to LaunchDescription
-    ld.add_action(create3_gazebo)
-    # Add AWS models to gazebo path
-    os.environ['GAZEBO_MODEL_PATH'] += os.pathsep + os.path.join(pkg_aws_house_dir, 'models')
+    # Add actions to LaunchDescription
+    ld.add_action(set_gazebo_model_path_env)
+    ld.add_action(world_spawn)
 
     return ld
