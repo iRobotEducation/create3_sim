0d82d202cbdf0ea634841e39d80326035badb22b
Alberto Soragna <alberto.soragna@gmail.com>
2021-10-20 15:13:23 +0100

    update license, readme and github badge
    Signed-off-by: Alberto Soragna <alberto.soragna@gmail.com>

--- irobot_create_toolbox/launch/teleop_joystick.launch.py
+++ irobot_create_toolbox/launch/teleop_joystick.launch.py
@@ -1,17 +1,5 @@
 #!/usr/bin/env python3
 # Copyright 2021 iRobot Corporation. All Rights Reserved.
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 # @author Alexis Pojomovsky (apojomovsky@irobot.com)
 #
 # Launches a joystick teleop node.
@@ -56,7 +44,7 @@ def generate_launch_description():
     joy_config = LaunchConfiguration('joy_config')
     joy_dev = LaunchConfiguration('joy_dev')
 
-    # Invokes a node that interfaces a generic joystick to ROS2.
+    # Invokes a node that interfaces a generic joystick to ROS 2.
     joy_node = Node(package='joy', executable='joy_node', name='joy_node',
                     parameters=[{
                         'dev': joy_dev,
