113593dbcba51fdb9e445d216516a503b37b3834
badr.moutalib <badr.moutalib@accenture.com>
2022-10-12 19:26:19 +0200

    cleanup unsused imports

--- irobot_create_ignition/irobot_create_ignition_bringup/launch/create3_ignition.launch.py
+++ irobot_create_ignition/irobot_create_ignition_bringup/launch/create3_ignition.launch.py
@@ -10,10 +10,8 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
-from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import Node
 
 ARGUMENTS = [
     DeclareLaunchArgument('bridge', default_value='true',
